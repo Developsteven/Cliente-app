4546889d912c8149f9810081c63f59998ac4c9f9
Developsteven <steven.perez099@gmail.com>
2021-03-23 18:06:42 -0500

    Agregando los mensajes de errores en la plantilla form

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -12,6 +12,7 @@ export class FormComponent implements OnInit {
 
   public cliente: Cliente = new Cliente();
   public titulo: string = "Crear Cliente";
+  public errores: string[];
 
   constructor(private clienteService: ClienteService, 
     private router: Router,
@@ -34,6 +35,11 @@ export class FormComponent implements OnInit {
       cliente =>{ 
       this.router.navigate(['/clientes'])
       swal.fire('Nuevo cliente', `El cliente ${cliente.nombre} ha sido creado con exito`, 'success')
+    },
+    err => {
+      this.errores = err.error.errors as string[];
+      console.error('El codigo del error desd el backend: ' + err.status);
+      console.error(err.error.errors);
     }
     );
   }
@@ -43,6 +49,11 @@ export class FormComponent implements OnInit {
       json => {
         this.router.navigate(['/clientes'])
         swal.fire('Cliente Actualizado', `${json.mensaje}: ${json.cliente.nombre}`, 'success')
+      },
+      err => {
+        this.errores = err.error.errors as string[];
+        console.error('El codigo del error desd el backend: ' + err.status);
+        console.error(err.error.errors);
       }
     );
   }  
