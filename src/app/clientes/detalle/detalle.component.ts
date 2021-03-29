d6491250835c81d9e874a0696f0cbd0f1f610c12
Developsteven <steven.perez099@gmail.com>
2021-03-29 16:58:34 -0500

    Validando imagen y dando estilo con barra de progreso.

--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -1,44 +1,62 @@
+import { HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
 import { Cliente } from '../cliente';
-import { ClienteService} from '../cliente.service';
+import { ClienteService } from '../cliente.service';
 
 @Component({
   selector: 'detalle-cliente',
   templateUrl: './detalle.component.html',
-  styleUrls: ['./detalle.component.css']
+  styleUrls: ['./detalle.component.css'],
 })
 export class DetalleComponent implements OnInit {
-
   cliente: Cliente;
-  titulo: string ="Detalle del cliente";
+  titulo: string = 'Detalle del cliente';
   private fotoSeleccionada: File;
+  progreso: number = 0;
 
-  constructor(private clienteService: ClienteService, 
-    private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private clienteService: ClienteService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
-  ngOnInit(){
-    this.activatedRoute.paramMap.subscribe(params => {
+  ngOnInit() {
+    this.activatedRoute.paramMap.subscribe((params) => {
       let id: number = +params.get('id');
-      if(id){
-        this.clienteService.getCliente(id).subscribe(cliente => {
+      if (id) {
+        this.clienteService.getCliente(id).subscribe((cliente) => {
           this.cliente = cliente;
         });
       }
     });
   }
 
-
-  seleccionarFoto(event){
+  seleccionarFoto(event) {
     this.fotoSeleccionada = event.target.files[0];
+    this.progreso = 0;
     console.log(this.fotoSeleccionada);
+    if (this.fotoSeleccionada.type.indexOf('image') < 0) {
+      Swal.fire('Error seleccionar imagen: ','El archivo debe de ser del tipo imagen','error');
+      this.fotoSeleccionada = null;
+    }
   }
 
-  subirFoto(){
-    this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id).subscribe(cliente =>{
-      this.cliente = cliente;
-      Swal.fire('la foto se ha subido completamente!', `La foto se ha subido con exito: ${this.cliente.foto}`, 'success');
-    });
+  subirFoto() {
+    if (!this.fotoSeleccionada) {
+      Swal.fire('Error Upload: ', 'Debe seleccionar una foto', 'error');
+    } else {
+      this.clienteService
+        .subirFoto(this.fotoSeleccionada, this.cliente.id)
+        .subscribe(event => {
+          if (event.type === HttpEventType.UploadProgress){
+            this.progreso = Math.round((event.loaded / event.total)*100);
+          }else if(event.type === HttpEventType.Response){
+            let response: any = event.body;
+            this.cliente = response.cliente as Cliente;
+            Swal.fire('la foto se ha subido completamente!', response.mensaje,'success');
+          }
+        });
+    }
   }
 }
