d972af9060987001999bb9abad01ae1e0309c8e8
Developsteven <steven.perez099@gmail.com>
2021-04-09 16:41:54 -0500

    Creando clase usuario.

--- src/app/usuarios/login.component.ts
+++ src/app/usuarios/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Usuario } from './usuario';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   titulo:string = 'Por favor Sing In!';
+  usuario: Usuario;
 
-  constructor() { }
+  constructor() {
+    this.usuario = new Usuario();
+   }
 
   ngOnInit(): void {
+  }
+
+  login(): void{
+    console.log(this.usuario);
+    if(this.usuario.username == null || this.usuario.password == null){
+      Swal.fire('Error Login', 'Username o password vacias!', 'error')
+      return;
+    }
   }
 
 }
