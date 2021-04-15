e1a51180572d86ef3a491d252f79b278dfa211bf
Developsteven <steven.perez099@gmail.com>
2021-04-15 12:20:22 -0500

    Añadiendo autocomplete con Angular Material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { PaginatorComponent } from './paginator/paginator.component';
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { registerLocaleData } from '@angular/common';
 import LocalES from '@angular/common/locales/es';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,6 +25,10 @@ import { TokenInterceptor } from './usuarios/interceptors/token.interceptor';
 import { AuthInterceptor } from './usuarios/interceptors/auth.interceptor';
 import { DetalleFacturaComponent } from './facturas/detalle-factura.component';
 import { FacturasComponent } from './facturas/facturas.component';
+
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 registerLocaleData(LocalES, 'es');
 
@@ -72,6 +76,10 @@ const routes: Routes = [
     BrowserAnimationsModule,
     MatDatepickerModule,
     MatMomentDateModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
   ],
   providers: [
     ClienteService,
--- src/app/facturas/facturas.component.ts
+++ src/app/facturas/facturas.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 import { ClienteService } from '../clientes/cliente.service';
 import { Factura } from './models/factura';
 
@@ -12,6 +15,10 @@ export class FacturasComponent implements OnInit {
   titulo: string = 'Nueva Factura';
   factura: Factura = new Factura();
 
+  autocompleteControl = new FormControl();
+  productos: string[] = ['Samsumg', 'Gorpo', 'Tripode', 'Smarphone' ];
+  productosFiltrados: Observable<string[]>;
+
   constructor(private clienteService: ClienteService,
     private activatedRoute: ActivatedRoute) {}
 
@@ -20,5 +27,18 @@ export class FacturasComponent implements OnInit {
       let clienteId = +params.get('clienteId');
       this.clienteService.getCliente(clienteId).subscribe(cliente => this.factura.cliente = cliente);
     });
+
+    this.productosFiltrados = this.autocompleteControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.productos.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
